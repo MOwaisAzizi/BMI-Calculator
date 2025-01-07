8ba2225baa6b9681f5cef7ce26c7f0f1e92a6a3c
owias azizi <owaisazizi360@gmail.com>
2025-01-07 19:24:32 +0430

    progressing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react"
 
 function App(){
-  return <h1>som text</h1>
+
+  const [hight,setHight] = useState(0)
+  const [weight,setWeight] = useState(0)
+  const [result,setResutl] = useState(0)
+  const [catagory,setCatagory] = useState('')
+
+  function handleOperation(){
+    setResutl(weight / ((hight/100) * (hight/100)))
+  }
+
+  useEffect(function(){
+    if(result < 17.1 && result >= 15 ) setCatagory('Underweight')
+      else if(result >= 17.1 && result < 23.2 ) setCatagory('Normal')
+      else if(result >= 23.2 && result < 38 ) setCatagory('overweight')
+      else if( result < 15 && result >= 38 ) setCatagory('uncurrect input detected')
+  },[result])
+
+  return (
+    <div className="container">
+      <div className="form">
+        <div className="form-group" >
+            <label>Hight</label>
+            <input type="number" id="hight" onChange={(e)=>setHight(e.target.value)} placeholder="cm" required/>
+         </div>
+        <div className="form-group">
+            <label>Weight</label>
+            <input type="number" id="weight" onChange={(e)=>setWeight(e.target.value)} placeholder="kg" required/>
+        </div>
+        <button type="submit" onClick={handleOperation}>Result</button>
+        <h2>{result}</h2>
+        <h2>{catagory}</h2>
+    </div>
+</div>
+  )
 }
 export default App
